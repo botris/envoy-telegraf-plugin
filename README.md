11530606c923423e0a576aed6c0e803a98605536
botris <me@borissondagh.com>
2023-07-03 15:48:43 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Envoy Telegraf Input Plugin
+
+Gather data from a 3-phase [Enphase Envoy](https://enphase.com/store/communication/envoy-s-metered). <br />
+Based on [Telegraf Execd Go Shim](https://github.com/influxdata/telegraf/tree/master/plugins/common/shim).
+
+### Configuration
+
+```toml
+[[inputs.envoy]]
+    # Envoy management url, replace with IP address if you have assigned your envoy a static IP.
+    envoy_url = "http://envoy.local"
+```
+
+### Metrics
+
+- envoy
+    - fields:
+        - total (int)
+        - p1_production (int)
+        - p1_consumption (int)
+        - p1_net (int)
+        - p2_production (int)
+        - p2_consumption (int)
+        - p2_net (int)
+        - p3_production (int)
+        - p3_consumption (int)
+        - p3_net (int)
+
+### Example Output
+
+```
+envoy total=939i,p1_consumption=18i,p1_net=-699i,p2_net=-707i,p1_production=718i,p2_production=722i,p2_consumption=14i,p3_production=718i,p3_consumption=3065i,p3_net=2346i 1688391414688034814
+envoy p2_production=712i,p3_production=707i,p1_consumption=18i,p1_net=-689i,p2_consumption=15i,p2_net=-696i,p3_consumption=3059i,p3_net=2352i,total=966i,p1_production=708i 1688391415687653261
+```
+
+### Build local 
+git clone git@github.com:botris/envoy-telegraf-plugin.git
+go build -o build/envoy cmd/main.go
+./build/envoy -config plugin.conf 
+
---
+++ plugins/inputs/envoy/envoy.go
@@ -0,0 +1,144 @@
+package envoy
+
+import (
+	"encoding/json"
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+const endpoint = "/production.json?details=1"
+
+type Config struct {
+	Url string `toml:"envoy_url"`
+}
+
+type EnergyGrid struct {
+	Production  Energy
+	Consumption Energy
+	Net         Energy
+}
+
+type Energy struct {
+	Watt   int
+	Phases [3]int
+}
+
+type envoyResponse struct {
+	Production  []envoyEnergy `json:"production,omitempty"`
+	Consumption []envoyEnergy `json:"consumption,omitempty"`
+}
+
+type envoyEnergy struct {
+	Type            string       `json:"type"`
+	MeasurementType string       `json:"measurementType"`
+	WNow            float64      `json:"wNow"`
+	Phases          []envoyPhase `json:"Lines"`
+}
+
+type envoyPhase struct {
+	WNow float64 `json:"wNow"`
+}
+
+func init() {
+	inputs.Add("envoy", func() telegraf.Input {
+		return &Config{}
+	})
+}
+
+const sampleConfig = `
+  ## Config management url.
+  # envoy_url = "http://envoyResponse.local"
+`
+
+func (e Config) SampleConfig() string {
+	return sampleConfig
+}
+
+func (e Config) Gather(accumulator telegraf.Accumulator) error {
+	energyGrid := EnergyGrid{}
+
+	now := time.Now()
+	tags := map[string]string{}
+	fields := map[string]interface{}{
+		"total":          nil,
+		"p1_production":  nil,
+		"p1_consumption": nil,
+		"p1_net":         nil,
+		"p2_production":  nil,
+		"p2_consumption": nil,
+		"p2_net":         nil,
+		"p3_production":  nil,
+		"p3_consumption": nil,
+		"p3_net":         nil,
+	}
+	url := strings.TrimSuffix(e.Url, "/")
+
+	httpResp, err := http.Get(url + endpoint)
+	if err != nil {
+		//Envoy sometimes loses LAN connection, send nil values to reflect that.
+		accumulator.AddFields("envoy", fields, tags, now)
+
+		return nil
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+		}
+	}(httpResp.Body)
+
+	body, err := io.ReadAll(httpResp.Body)
+	if err != nil {
+		return err
+	}
+
+	request := envoyResponse{}
+	_ = json.Unmarshal(body, &request)
+
+	for _, prod := range request.Production {
+		if prod.Type == "eim" {
+			updateResponse(&energyGrid.Production, prod)
+		}
+	}
+
+	for _, cons := range request.Consumption {
+		if cons.MeasurementType == "total-consumption" {
+			updateResponse(&energyGrid.Consumption, cons)
+		}
+		if cons.MeasurementType == "net-consumption" {
+			updateResponse(&energyGrid.Net, cons)
+		}
+	}
+
+	fields = map[string]interface{}{
+		"total":          energyGrid.Net.Watt,
+		"p1_production":  energyGrid.Production.Phases[0],
+		"p1_consumption": energyGrid.Consumption.Phases[0],
+		"p1_net":         energyGrid.Net.Phases[0],
+		"p2_production":  energyGrid.Production.Phases[1],
+		"p2_consumption": energyGrid.Consumption.Phases[1],
+		"p2_net":         energyGrid.Net.Phases[1],
+		"p3_production":  energyGrid.Production.Phases[2],
+		"p3_consumption": energyGrid.Consumption.Phases[2],
+		"p3_net":         energyGrid.Net.Phases[2],
+	}
+
+	accumulator.AddFields("envoy", fields, tags, now)
+
+	return nil
+}
+
+func (e Config) Description() string {
+	return "Gather consumption information from a three-phase envoyResponse."
+}
+
+func updateResponse(energyResponse *Energy, energyRequest envoyEnergy) {
+	energyResponse.Watt = int(energyRequest.WNow)
+	for key, phase := range energyRequest.Phases {
+		energyResponse.Phases[key] = int(phase.WNow)
+	}
+}
